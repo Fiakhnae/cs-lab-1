329782526ba6a11aa06bc8b0feee9c1800312ecd
Fiakhnae <d.garmash.ua@gmail.com>
2024-10-28 01:17:24 +0200

    lab1 fixed

--- ViewModels/TestViewModel.cs
+++ ViewModels/TestViewModel.cs
@@ -20,179 +20,28 @@ namespace CSLab1.ViewModels {
             await Shell.Current.DisplayAlert("Успішно", "Успішно тести пройдені", "ОК");
         }
         async Task Work() {
-            new Thread(async () => {
-                int count1 = 0;
-                int count2 = 0;
-                int count3 = 0;
-                Task task1 = Task.Delay(10000);
-                Task task2 = Task.Run(() => {
-                    Random random = new Random();
-                    int step;
-                    while (true) {
-                        step = random.Next(0, 101);
-                        if (step >= Singleton.Processors[0].Random) {
-                            continue;
-                        }
-                        Algorithm.Func1();
-                        count1++;
-                        step = random.Next(0, 101);
-                        if (step >= Singleton.Processors[0].Random) {
-                            continue;
-                        }
-                        Algorithm.Func2();
-                        count2++;
-                        step = random.Next(0, 101);
-                        if (step >= Singleton.Processors[0].Random) {
-                            continue;
-                        }
-                        Algorithm.Func3();
-                        count3++;
-                        step = random.Next(0, 101);
-                        if (step >= Singleton.Processors[0].Random) {
-                            continue;
-                        }
-                    }
-                });
-                await Task.WhenAny(task1, task2);
-                await MainThread.InvokeOnMainThreadAsync(async () => {
-                    await Shell.Current.DisplayAlert("Результат",
-                        $"Процесор №1\nTask1: {count1}\nTask2: {count2}\nTask3: {count3}\n", "OK");
-                });
-
-            }) { IsBackground = true }.Start();
+            CreateThread("1");
             await Task.Delay(1000);
-            new Thread(async () => {
-                int count1 = 0;
-                int count2 = 0;
-                int count3 = 0;
-                Task task1 = Task.Delay(10000);
-                Task task2 = Task.Run(() => {
-                    Random random = new Random();
-                    int step;
-                    while (true) {
-                        step = random.Next(0, 101);
-                        if (step >= Singleton.Processors[0].Random) {
-                            continue;
-                        }
-                        Algorithm.Func1();
-                        count1++;
-                        step = random.Next(0, 101);
-                        if (step >= Singleton.Processors[0].Random) {
-                            continue;
-                        }
-                        Algorithm.Func2();
-                        count2++;
-                        step = random.Next(0, 101);
-                        if (step >= Singleton.Processors[0].Random) {
-                            continue;
-                        }
-                        Algorithm.Func3();
-                        count3++;
-                        step = random.Next(0, 101);
-                        if (step >= Singleton.Processors[0].Random) {
-                            continue;
-                        }
-                    }
-                });
-                await Task.WhenAny(task1, task2);
-                await MainThread.InvokeOnMainThreadAsync(async () => {
-                    await Shell.Current.DisplayAlert("Результат",
-                        $"Процесор №2\nTask1: {count1}\nTask2: {count2}\nTask3: {count3}\n", "OK");
-                });
-
-            }) { IsBackground = true }.Start();
+            CreateThread("2");
             await Task.Delay(1000);
-            new Thread(async () => {
-                int count1 = 0;
-                int count2 = 0;
-                int count3 = 0;
-                Task task1 = Task.Delay(10000);
-                Task task2 = Task.Run(() => {
-                    Random random = new Random();
-                    int step;
-                    while (true) {
-                        step = random.Next(0, 101);
-                        if (step >= Singleton.Processors[0].Random) {
-                            continue;
-                        }
-                        Algorithm.Func1();
-                        count1++;
-                        step = random.Next(0, 101);
-                        if (step >= Singleton.Processors[0].Random) {
-                            continue;
-                        }
-                        Algorithm.Func2();
-                        count2++;
-                        step = random.Next(0, 101);
-                        if (step >= Singleton.Processors[0].Random) {
-                            continue;
-                        }
-                        Algorithm.Func3();
-                        count3++;
-                        step = random.Next(0, 101);
-                        if (step >= Singleton.Processors[0].Random) {
-                            continue;
-                        }
-                    }
-                });
-                await Task.WhenAny(task1, task2);
-                await MainThread.InvokeOnMainThreadAsync(async () => {
-                    await Shell.Current.DisplayAlert("Результат",
-                        $"Процесор №3\nTask1: {count1}\nTask2: {count2}\nTask3: {count3}\n", "OK");
-                });
-
-            }) { IsBackground = true }.Start();
+            CreateThread("3");
             await Task.Delay(1000);
-            new Thread(async () => {
-                int count1 = 0;
-                int count2 = 0;
-                int count3 = 0;
-                Task task1 = Task.Delay(10000);
-                Task task2 = Task.Run(() => {
-                    Random random = new Random();
-                    int step;
-                    while (true) {
-                        step = random.Next(0, 101);
-                        if (step >= Singleton.Processors[0].Random) {
-                            continue;
-                        }
-                        Algorithm.Func1();
-                        count1++;
-                        step = random.Next(0, 101);
-                        if (step >= Singleton.Processors[0].Random) {
-                            continue;
-                        }
-                        Algorithm.Func2();
-                        count2++;
-                        step = random.Next(0, 101);
-                        if (step >= Singleton.Processors[0].Random) {
-                            continue;
-                        }
-                        Algorithm.Func3();
-                        count3++;
-                        step = random.Next(0, 101);
-                        if (step >= Singleton.Processors[0].Random) {
-                            continue;
-                        }
-                    }
-                });
-                await Task.WhenAny(task1, task2);
-                await MainThread.InvokeOnMainThreadAsync(async () => {
-                    await Shell.Current.DisplayAlert("Результат",
-                        $"Процесор №4\nTask1: {count1}\nTask2: {count2}\nTask3: {count3}\n", "OK");
-                });
-
-            }) { IsBackground = true }.Start();
+            CreateThread("4");
             await Task.Delay(1000);
+            CreateThread("5");
+        }
+        void CreateThread(string id) {
             new Thread(async () => {
                 int count1 = 0;
                 int count2 = 0;
                 int count3 = 0;
+                int operations = 0;
                 Task task1 = Task.Delay(10000);
                 Task task2 = Task.Run(() => {
                     Random random = new Random();
                     int step;
                     while (true) {
+                        operations += 3;
                         step = random.Next(0, 101);
                         if (step >= Singleton.Processors[0].Random) {
                             continue;
@@ -215,13 +64,15 @@ namespace CSLab1.ViewModels {
                         if (step >= Singleton.Processors[0].Random) {
                             continue;
                         }
-
                     }
                 });
                 await Task.WhenAny(task1, task2);
                 await MainThread.InvokeOnMainThreadAsync(async () => {
+                    int count = count1 + count2 + count3;
                     await Shell.Current.DisplayAlert("Результат",
-                        $"Процесор №5\nTask1: {count1}\nTask2: {count2}\nTask3: {count3}\n", "OK");
+                        $"Процесор №{id}\nTask: {count1}\nTask2: {count2}\nTask3: {count3}" +
+                        $"\nFinished Operations: {count}/{operations}" +
+                        $"\nEfficiency: {Math.Round((double)count / operations * 100, 5)}%", "OK");
                 });
 
             }) { IsBackground = true }.Start();
